d03bbf2ca540e6480671c19200a2cffe3ce960bd
Sergio De la torre <106643330+Sdelatorre123@users.noreply.github.com>
2022-11-21 01:11:39 -0600

    Add files via upload

---
+++ Develop/script.js
@@ -0,0 +1,37 @@
+// Assignment code here
+var characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbols = ['!@#$%^&*()_-+='];
+
+
+
+// function that draws random characters from the array
+function draw(group) {
+  var randomNumber = Math.floor(Math.random() * group.length);
+  return group[randomNumber];
+}
+
+//function to ask prompt questions for user prefrence
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like to have on your password?");
+  chooseLength();
+  if (passwordLength > 8 && passwordLength < 129) {
+    chooseCharacters();
+  }
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-# javascript-password-generator
-
-Welcome! do you need a strong password? but can't think of one? I can help with that!
-
-with this generator it create and an amazing and strong password for your needs!
-
-![Screenshot_20221121_010307](https://user-images.githubusercontent.com/106643330/202986883-35147590-660a-4702-beda-e6caf49b5938.png)
-
-
-Tools that helped create this:
-- HTML
-- CSS
-- Javascript
+# Password Generator Starter Code
